825d5ec883066935205b2adefe59b921a12cd182
kohdc <kohdc1723@gmail.com>
2023-10-31 16:19:46 -0700

    separate backend code to new repo cooking-home-backend

--- frontend/src/features/preference/components/index.js
+++
@@ -1,9 +0,0 @@
-import PreferenceSetting from "./PreferenceSetting";
-import PreferenceSettingForm from "./PreferenceSettingForm";
-import NewPreference from "./NewPreference";
-
-export {
-    PreferenceSetting,
-    PreferenceSettingForm,
-    NewPreference
-};
